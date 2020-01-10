3b253925c5b5c3d637fdc74edcf77b90a28191f1
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2020-01-10 10:27:52 +0100

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+import java.util.stream.Stream;
+
+public class Main {
+
+    Stream<>
+}
---
+++ src/main/java/Pupils.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class Pupils {
+
+    private int id;
+    private String lastName;
+    private String firstName;
+    private Date dateOfBirth;
+    private int postcode;
+    private int city;
+
+}
