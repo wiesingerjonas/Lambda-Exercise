060847da6d5ac5518c249611556acc9a3aef9b01
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2020-01-10 10:28:41 +0100

    Getter und Setter implementiert

--- src/main/java/Pupils.java
+++ src/main/java/Pupils.java
@@ -19,4 +19,56 @@ public class Pupils {
         this.city = city;
     }
 
+
+    //region GETTER
+    public int getId() {
+        return id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public int getPostcode() {
+        return postcode;
+    }
+
+    public int getCity() {
+        return city;
+    }
+    //endregion
+
+    //region SETTER
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setPostcode(int postcode) {
+        this.postcode = postcode;
+    }
+
+    public void setCity(int city) {
+        this.city = city;
+    }
+    //endregion
 }
