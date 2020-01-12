288616e68f53371b04b327437507b3be271c3c82
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2020-01-12 15:05:07 +0100

    readFile(); wurde initialisiert und gibt den Inhalt der Datei aus

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,24 @@
-import java.util.stream.Stream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Main {
 
-    Stream<>
+    private static String FILENAME = "pupuls.csv";
+
+    public static void main(String[] args) {
+
+        readFile();
+
+
+    }
+
+    private static void readFile() {
+        try {
+            System.out.println(Files.readAllLines(Paths.get(FILENAME)).get(0));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
