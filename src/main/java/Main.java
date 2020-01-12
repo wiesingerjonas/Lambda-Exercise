acca5eb6b8c74fcb4a8c411384bd2bd83c5c4009
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2020-01-12 15:19:35 +0100

    readFile() processed the data from file and returns it as List<Pupil>

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,10 @@
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -8,17 +12,33 @@ public class Main {
 
     public static void main(String[] args) {
 
-        readFile();
-
+        List<Pupil> pupils = readFile();
 
     }
 
-    private static void readFile() {
+    private static List<Pupil> readFile() {
+
+        List<Pupil> pupils = new ArrayList<>();
+
         try {
-            System.out.println(Files.readAllLines(Paths.get(FILENAME)).get(0));
+            List<String> lines = Files.readAllLines(Paths.get(FILENAME));
+
+            for (int i = 1; i < lines.size(); i++) {
+                String[] lineParts = lines.get(i).split(";");
+                pupils.add(new Pupil(Integer.parseInt(lineParts[0]),
+                                    lineParts[1],
+                                    lineParts[2],
+                                    lineParts[3],
+                                    Integer.parseInt(lineParts[4]), lineParts[5]));
+
+            }
+
+            return pupils;
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        return null;
     }
 
 }
